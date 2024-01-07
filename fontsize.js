d5cd48d94c37f9824d640efdd6497be7175bfe44
HetBlogBoek <128465455+HetBlogBoek@users.noreply.github.com>
2024-01-07 14:35:44 +0100

    Add files via upload

---
+++ fontsize.js
@@ -0,0 +1,11 @@
+document.getElementById('fontBig').addEventListener('click', function() {
+    document.body.style.fontSize = '1.8rem';
+});
+
+document.getElementById('fontMedium').addEventListener('click', function() {
+    document.body.style.fontSize = '1.4rem';
+});
+
+document.getElementById('fontSmall').addEventListener('click', function() {
+    document.body.style.fontSize = '1.0rem';
+});
