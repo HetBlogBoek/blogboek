a6f0e559e8918e33e167dd0a4c7506cdd02377a6
HetBlogBoek <128465455+HetBlogBoek@users.noreply.github.com>
2024-01-07 14:51:52 +0100

    Update fontsize.js

--- fontsize.js
+++ fontsize.js
@@ -1,11 +1,32 @@
+let isMobile = window.matchMedia("(max-width: 1000px)")
+let bigFontDesktop = '1.8rem';
+let mediumFontDesktop = '1.4rem';
+let smallFontDesktop = '1rem';
+let bigFontMobile = '3.3rem';
+let mediumFontMobile = '2.8rem';
+let smallFontMobile = '2.0rem';
+
 document.getElementById('fontBig').addEventListener('click', function() {
-    document.body.style.fontSize = '1.8rem';
+    if(isMobile.matches){
+        document.body.style.fontSize = bigFontMobile;
+    }else{
+        console.log("aub")
+        document.body.style.fontSize = bigFontDesktop;
+    }
 });
 
 document.getElementById('fontMedium').addEventListener('click', function() {
-    document.body.style.fontSize = '1.4rem';
+    if(isMobile.matches){
+        document.body.style.fontSize = mediumFontMobile;
+    }else{
+        document.body.style.fontSize = mediumFontDesktop;
+    }
 });
 
 document.getElementById('fontSmall').addEventListener('click', function() {
-    document.body.style.fontSize = '1.0rem';
+    if(isMobile.matches){
+        document.body.style.fontSize = smallFontMobile;
+    }else{
+        document.body.style.fontSize = smallFontDesktop;
+    }
 });
